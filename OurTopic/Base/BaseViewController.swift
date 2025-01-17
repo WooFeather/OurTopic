18e99f9c60b835b14a4448ada8f1e74c95622721
Woohyun Jo <bluewing1004@gmail.com>
2025-01-17 15:47:52 +0900

    [Setting] 프로젝트 기본세팅

---
+++ OurTopic/Base/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  OurTopic
+//
+//  Created by 조우현 on 1/17/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+    func configureView() { }
+}
--- OurTopic/SearchPhoto/SearchPhotoViewController.swift
+++ OurTopic/SearchPhoto/SearchPhotoViewController.swift
@@ -7,24 +7,17 @@
 
 import UIKit
 
-class SearchPhotoViewController: UIViewController {
+class SearchPhotoViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func configureHierarchy() {
         
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func configureLayout() {
+        
     }
-    */
-
+    
+    override func configureView() {
+        view.backgroundColor = .blue
+    }
 }
